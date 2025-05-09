8e3139ec85b6af0b9745a8266258641508f167c2
Eric Gao <ericxgao@gmail.com>
2025-05-09 10:12:15 -0700

    feat: add some more sapphire modules

---
+++ plugin.cpp
@@ -0,0 +1,27 @@
+#include "plugin.hpp"
+
+Plugin *pluginInstance;
+
+__attribute__ ((visibility ("default")))
+void init(Plugin *p) {
+  pluginInstance = p;
+
+  p->addModel(modelSapphireElastika);
+  p->addModel(modelSapphireFrolic);
+  p->addModel(modelSapphireGalaxy);
+  p->addModel(modelSapphireGlee);
+  p->addModel(modelSapphireGravy);
+  p->addModel(modelSapphireHiss);
+  // p->addModel(modelSapphireChaops);
+  // p->addModel(modelSapphireMoots);
+  // p->addModel(modelSapphireNucleus);
+  // p->addModel(modelSapphirePivot);
+  // p->addModel(modelSapphirePolynucleus);
+  // p->addModel(modelSapphirePop);
+  // p->addModel(modelSapphireRotini);
+  // p->addModel(modelSapphireSam);
+  // p->addModel(modelSapphireTin);
+  // p->addModel(modelSapphireTout);
+  // p->addModel(modelSapphireTricorder);
+  // p->addModel(modelSapphireTubeUnit);
+}
